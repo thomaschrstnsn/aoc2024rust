904d6eef36d6dfc66b92f9695d79f9408aec9030
Thomas Christensen <tfc@lindcapital.com>
2024-12-03 08:52:32 +0100

    day02: part two

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -58,8 +58,30 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(parsed.iter().filter(|r| is_safe(r)).count())
 }
 
+fn is_safe_with_problem_dampener(r: &[i64]) -> bool {
+    if is_safe(r) {
+        return true;
+    }
+
+    for i in 0..r.len() {
+        let (left, right) = r.split_at(i);
+        let (_, skipped) = right.split_at(1);
+        let mutation = [left, skipped].concat();
+        if is_safe(&mutation) {
+            return true;
+        }
+    }
+    false
+}
+
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let parsed = parse(input);
+    Some(
+        parsed
+            .iter()
+            .filter(|r| is_safe_with_problem_dampener(r))
+            .count(),
+    )
 }
 
 #[cfg(test)]
@@ -103,8 +125,45 @@ mod tests {
     }
 
     #[test]
+    fn test_is_safe_with_problem_dampener_1() {
+        assert!(is_safe_with_problem_dampener(&[7, 6, 4, 2, 1]));
+    }
+
+    #[test]
+    fn test_is_safe_with_problem_dampener_2() {
+        assert!(!is_safe_with_problem_dampener(&[1, 2, 7, 8, 9]));
+    }
+
+    #[test]
+    fn test_is_safe_with_problem_dampener_3() {
+        assert!(!is_safe_with_problem_dampener(&[9, 7, 6, 2, 1]));
+    }
+
+    #[test]
+    fn test_is_safe_with_problem_dampener_4() {
+        assert!(is_safe_with_problem_dampener(&[1, 3, 2, 4, 5]));
+    }
+
+    #[test]
+    fn test_is_safe_with_problem_dampener_5() {
+        assert!(is_safe_with_problem_dampener(&[8, 6, 4, 4, 1]));
+    }
+
+    #[test]
+    fn test_is_safe_with_problem_dampener_6() {
+        assert!(is_safe_with_problem_dampener(&[1, 3, 6, 7, 9]));
+    }
+
+    #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
+    }
+
+    #[test]
+    fn test_split_at() {
+        let (left, right) = [1, 2, 3].split_at(0);
+        assert_eq!(left, []);
+        assert_eq!(right, [1, 2, 3]);
     }
 }
