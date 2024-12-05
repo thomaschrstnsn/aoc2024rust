182a013f8dc065b2f34505186c428fde30a933e9
Thomas Christensen <thomas@chrstnsn.dk>
2024-12-05 17:28:59 +0100

    day04: part one

---
+++ src/bin/04.rs
@@ -0,0 +1,121 @@
+use itertools::Itertools;
+
+advent_of_code::solution!(4);
+
+#[derive(Debug)]
+struct Input<'a>(Vec<&'a str>);
+
+#[derive(Debug)]
+struct Vec2<T>(T, T);
+
+impl Vec2<usize> {
+    fn add_signed(&self, other: &Vec2<isize>) -> Self {
+        Vec2(
+            self.0.overflowing_add_signed(other.0).0,
+            self.1.overflowing_add_signed(other.1).0,
+        )
+    }
+}
+
+impl Vec2<isize> {
+    fn mul(&self, n: isize) -> Self {
+        Vec2(self.0 * n, self.1 * n)
+    }
+}
+
+type Direction = Vec2<isize>;
+type Point = Vec2<usize>;
+
+impl<'a> Input<'a> {
+    fn from_str(input: &'a str) -> Option<Self> {
+        let lines: Vec<&str> = input.lines().collect();
+
+        let lengths = lines.iter().map(|l| l.len()).dedup().count();
+        if lengths != 1 {
+            return None;
+        }
+
+        Some(Self(lines))
+    }
+
+    fn columns(&self) -> usize {
+        self.0.first().unwrap().len()
+    }
+    fn rows(&self) -> usize {
+        self.0.len()
+    }
+
+    fn get(&self, p: &Point) -> Option<char> {
+        self.0.get(p.0)?.chars().nth(p.1)
+    }
+
+    fn get_n_chars_in_direction(
+        &self,
+        n: usize,
+        start: &Point,
+        direction: &Direction,
+    ) -> Vec<char> {
+        (0..)
+            .map(|i| start.add_signed(&direction.mul(i)))
+            .map(|p| self.get(&p))
+            .take_while(|c| c.is_some())
+            .map(|c| c.unwrap())
+            .take(n)
+            .collect()
+    }
+}
+
+fn x_marks_the_spot(point: &Point, input: &Input) -> usize {
+    const DIRECTIONS: &[Direction] = &[
+        Vec2(1, 0),
+        Vec2(1, 1),
+        Vec2(0, 1),
+        Vec2(-1, 1),
+        Vec2(-1, 0),
+        Vec2(-1, -1),
+        Vec2(0, -1),
+        Vec2(1, -1),
+    ];
+    const TARGET: &[char] = &['X', 'M', 'A', 'S'];
+    if input.get(point).unwrap() == 'X' {
+        return DIRECTIONS
+            .iter()
+            .map(|d| input.get_n_chars_in_direction(TARGET.len(), point, d))
+            .filter(|v| v == TARGET)
+            .count();
+    }
+    0
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let input = Input::from_str(input)?;
+
+    let mut sum = 0;
+    for x in 0..input.columns() {
+        for y in 0..input.rows() {
+            sum += x_marks_the_spot(&Vec2(x, y), &input);
+        }
+    }
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(18));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
