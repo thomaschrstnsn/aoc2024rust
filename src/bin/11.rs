d4ac677d8edf400b9d63c9a280ae8c5ed45c3113
Thomas Christensen <thomas@chrstnsn.dk>
2024-12-13 22:30:21 +0100

    day11: part two (after refactor)

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,3 +1,7 @@
+use std::{collections::HashMap, mem};
+
+use itertools::Itertools;
+
 advent_of_code::solution!(11);
 
 fn parse(input: &str) -> Vec<usize> {
@@ -7,39 +11,67 @@ fn parse(input: &str) -> Vec<usize> {
         .collect()
 }
 
-fn blink_one(n: usize) -> Vec<usize> {
-    if n == 0 {
-        return vec![1];
-    }
-    let digits = n.to_string();
-    if digits.len() % 2 == 0 {
-        let (first, second) = digits.split_at(digits.len() / 2);
-        let (first, second) = (
-            first.parse().expect("number"),
-            second.parse().expect("number"),
-        );
-        return vec![first, second];
-    }
-    vec![n * 2024]
-}
+struct Solution(HashMap<usize, usize>);
 
-fn blink_once(nums: &[usize]) -> impl Iterator<Item = usize> + use<'_> {
-    nums.iter().flat_map(|num| blink_one(*num))
-}
-
-fn blink(mut nums: Vec<usize>, iters: usize) -> Vec<usize> {
-    for _ in 0..iters {
-        nums = blink_once(&nums).collect();
+impl Solution {
+    fn new(stones: &[usize]) -> Self {
+        Self(stones.iter().cloned().counts())
     }
-    nums
+
+    fn blink_times(&mut self, blinks: usize) {
+        for _ in 0..blinks {
+            self.blink_once();
+        }
+    }
+
+    fn blink_once(&mut self) {
+        let mut prev = HashMap::new();
+        mem::swap(&mut self.0, &mut prev);
+
+        for (stone, count) in prev.into_iter() {
+            if stone == 0 {
+                self.0.entry(1).and_modify(|c| *c += count).or_insert(count);
+                continue;
+            }
+            let digits = stone.to_string();
+            if digits.len() % 2 == 0 {
+                let (first, second) = digits.split_at(digits.len() / 2);
+                let (first, second) = (
+                    first.parse().expect("number"),
+                    second.parse().expect("number"),
+                );
+                self.0
+                    .entry(first)
+                    .and_modify(|c| *c += count)
+                    .or_insert(count);
+                self.0
+                    .entry(second)
+                    .and_modify(|c| *c += count)
+                    .or_insert(count);
+                continue;
+            }
+            self.0
+                .entry(stone * 2024)
+                .and_modify(|c| *c += count)
+                .or_insert(count);
+        }
+    }
+
+    fn result(&self) -> usize {
+        self.0.values().sum()
+    }
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    Some(blink(parse(input), 25).len())
+    let mut solution = Solution::new(&parse(input));
+    solution.blink_times(25);
+    Some(solution.result())
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    Some(blink(parse(input), 75).len())
+    let mut solution = Solution::new(&parse(input));
+    solution.blink_times(75);
+    Some(solution.result())
 }
 
 #[cfg(test)]
@@ -55,6 +87,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65601038650482));
     }
 }
